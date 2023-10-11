b571ea473da8ea64612a53d537d7404215f7c0c3
DanyCarter <danielcadenas88@gmail.com>
2023-10-11 10:34:56 +0200

    Agregue un mejor formato

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
         const width = parseFloat(document.getElementById("width").value);
         const height = parseFloat(document.getElementById("height").value);/* Los input siempre devuelven valores numericos por ellos hay q pasarlo a FLoat */
 
-        if(!isNaN(height) && !isNaN(width)) {
+        if (!isNaN(height) && !isNaN(width)) {
             const area = width * height;
-            const peri = (2*width) + (2*height);
-            const diag = (Math.sqrt(Math.pow(width,2))+(Math.pow(height,2)));
+            const peri = (2 * width) + (2 * height);
+            const diag = (Math.sqrt(Math.pow(width, 2)) + (Math.pow(height, 2)));
 
             resultContainer.innerHTML = `Área del rectángulo: ${area.toFixed(2)} <Br><br> Perímetro del rectángulo: ${peri.toFixed(2)} <br><br> Diagonal del rectángulo ${diag.toFixed(2)} `
-        
-        } else {
+
+        } else  {
             resultContainer.innerHTML = `Por favor ingrese valores validos`
-        } })
+        }
+    })
 })
