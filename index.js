ec8e97ccf3d441cc23cac7a0f31189bf75c86373
DanyCarter <danielcadenas88@gmail.com>
2023-10-11 10:35:56 +0200

    mejore los <br>

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const peri = (2 * width) + (2 * height);
             const diag = (Math.sqrt(Math.pow(width, 2)) + (Math.pow(height, 2)));
 
-            resultContainer.innerHTML = `Área del rectángulo: ${area.toFixed(2)} <Br><br> Perímetro del rectángulo: ${peri.toFixed(2)} <br><br> Diagonal del rectángulo ${diag.toFixed(2)} `
+            resultContainer.innerHTML = `Área del rectángulo: ${area.toFixed(2)} <br><br> Perímetro del rectángulo: ${peri.toFixed(2)} <br><br> Diagonal del rectángulo ${diag.toFixed(2)} `
 
         } else  {
             resultContainer.innerHTML = `Por favor ingrese valores validos`
