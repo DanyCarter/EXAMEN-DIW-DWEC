a676f20cd458934ed1d6a00669a7253a3400105d
DanyCarter <danielcadenas88@gmail.com>
2023-10-11 10:51:29 +0200

    Arregle el codigo de la diagonal

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!isNaN(height) && !isNaN(width)) {
             const area = width * height;
             const peri = (2 * width) + (2 * height);
-            const diag = (Math.sqrt(Math.pow(width, 2)) + (Math.pow(height, 2)));
+            const diag = Math.sqrt((Math.pow(height,2))+(Math.pow(width,2))).toFixed(2);
 
-            resultContainer.innerHTML = `Área del rectángulo: ${area.toFixed(2)} <br><br> Perímetro del rectángulo: ${peri.toFixed(2)} <br><br> Diagonal del rectángulo ${diag.toFixed(2)} `
+            resultContainer.innerHTML = `Área del rectángulo: ${area.toFixed(2)} <br><br> Perímetro del rectángulo: ${peri.toFixed(2)} <br><br> Diagonal del rectángulo ${diag}`
 
         } else  {
             resultContainer.innerHTML = `Por favor ingrese valores validos`
